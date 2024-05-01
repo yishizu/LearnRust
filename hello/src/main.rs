ffc868082b6672118f5e0602d556a52c753290e4
yishizu <48244588+yishizu@users.noreply.github.com>
2024-05-01 14:05:02 +0900

    Add Day01

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -4,22 +4,22 @@ fn main() {
     let a = 10;
     let b = 5;
     let area = area_of(a, b);
-    let x =3;
+    let x = 3;
     let y = 24;
     let z = 34;
     let volume = volume(x, y, z);
-    println!("Area is {}. Volume is {}",area, volume);
-    let tupple:(i32, i32) = (2,6);
-    let arr1:[f32;3] = [0.3,0.5,0.7];
-    println!("{},{}",tupple.0,tupple.1);
-    println!("{},{}",arr1[0],arr1[1]);
+    println!("Area is {}. Volume is {}", area, volume);
+    let tupple: (i32, i32) = (2, 6);
+    let arr1: [f32; 3] = [0.3, 0.5, 0.7];
+    println!("{},{}", tupple.0, tupple.1);
+    println!("{},{}", arr1[0], arr1[1]);
     greet();
 }
 
-fn area_of(x:i32, y:i32) -> i32{
-    return x*y;
+fn area_of(x: i32, y: i32) -> i32 {
+    return x * y;
 }
 
-fn volume(x:i32, y:i32, z:i32) -> i32{
-    return  x*y*z;
+fn volume(x: i32, y: i32, z: i32) -> i32 {
+    return x * y * z;
 }
--- hellobevy/my_bevy_game/src/main.rs
+++ hellobevy/my_bevy_game/src/main.rs
@@ -1,6 +1,102 @@
+use bevy::prelude::*;
+
+pub struct PeoplePlugin;
+
+impl Plugin for PeoplePlugin {
+    fn build(&self, app: &mut App) {
+        // add things to your app here
+        app.add_systems(Startup, setup)
+            .add_systems(Update, person_printer)
+            .add_systems(Update, person_with_jobs)
+            .add_systems(Update, person_with_jobs_and_job)
+            .add_systems(Update, person_ready_for_work);
+    }
+}
+
 fn main() {
-    let missiles = 8;
-    let ready =3;
-    
-    println!("Hello, world!{}{}", missiles, ready);
+    App::new().add_plugins((DefaultPlugins, PeoplePlugin)).run();
+}
+
+pub fn hello_world() {
+    println!("Hello, Bevy!");
+}
+
+pub fn setup(mut commands: Commands) {
+    commands.spawn((
+        Person {
+            name: "Alice".to_string(),
+            age: 30,
+        },
+        Employed { job: Job::Doctor },
+    ));
+
+    commands.spawn((
+        Person {
+            name: "Bob".to_string(),
+            age: 31,
+        },
+        Employed {
+            job: Job::FireFighter,
+        },
+    ));
+
+    commands.spawn((
+        Person {
+            name: "Charlie".to_string(),
+            age: 32,
+        },
+        Employed { job: Job::Teacher },
+    ));
+    commands.spawn((Person {
+        name: "Dave".to_string(),
+        age: 33,
+    },));
+}
+pub fn person_printer(query: Query<&Person>) {
+    for person in query.iter() {
+        println!("Person: {} is {} years old", person.name, person.age);
+    }
+}
+
+pub fn person_with_jobs(query: Query<&Person, With<Employed>>) {
+    for person in query.iter() {
+        println!("Person: {} has a job", person.name);
+    }
+}
+pub fn person_with_jobs_and_job(query: Query<(&Person, &Employed)>) {
+    for (person, employed) in query.iter() {
+        let job_name = match employed.job {
+            Job::Doctor => "Doctor",
+            Job::FireFighter => "Fire Fighter",
+            Job::Teacher => "Teacher",
+            Job::Programmer => "Programmer",
+            Job::Nurse => "Nurse",
+        };
+        println!("Person: {} has a job as a {:?}", person.name, job_name);
+    }
+}
+pub fn person_ready_for_work(query: Query<&Person, Without<Employed>>) {
+    for person in query.iter() {
+        println!("Person: {} is ready for work", person.name);
+    }
+}
+
+#[derive(Component)]
+pub struct Person {
+    pub name: String,
+    pub age: u8,
+}
+
+#[derive(Component)]
+pub struct Employed {
+    pub job: Job,
+}
+
+#[derive(Debug)]
+pub enum Job {
+    Doctor,
+    FireFighter,
+    Teacher,
+    Programmer,
+    Nurse,
 }
